9d76a41c726542991791bb9672266f109028ac08
cpheps <cphelps8907@gmail.com>
2024-12-05 08:48:33 -0800

    Problem two day one

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashMap;
 
 fn main() {
     let mut list_one: Vec<i32> = Vec::new();
@@ -20,6 +21,15 @@ fn main() {
     list_one.sort();
     list_two.sort();
 
+    // Get the list total
+    let total = get_list_total(&list_one, &list_two);
+    println!("Total distance: {}", total);
+
+    let similarity_score = get_list_similarity_score(&list_one, &list_two);
+    println!("Similarity score: {}", similarity_score);
+}
+
+fn get_list_total(list_one: &Vec<i32>, list_two: &Vec<i32>) -> i32 {
     let mut total: i32 = 0;
     for i in 0..list_one.len() {
         // Find the difference between the two lists
@@ -30,8 +40,28 @@ fn main() {
         // Add the absolute value to the total
         total += diff.abs();
     }
+    return total;
+}
 
-    println!("{}", total);
+fn get_list_similarity_score(list_one: &Vec<i32>, list_two: &Vec<i32>) -> i32 {
+    let mut occurrences: HashMap<i32, i32> = HashMap::new();
+
+    // Seed the list with the values from list_one and 0 values
+    for point in list_one {
+        occurrences.insert(*point, 0);
+    }
+
+    // Iterate over the second list and increment the values
+    for point in list_two {
+        occurrences.entry(*point).and_modify(|count| *count += 1);
+    }
+
+    let mut total: i32 = 0;
+    for (key, val) in occurrences.iter() {
+        total += key * val;
+    }
+
+    return total;
 }
 
 // The output is wrapped in a Result to allow matching on errors.
